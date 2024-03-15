5439a564655a65719d82d8d17214465e876ea333
Cedric Hopf <hopf.cedric@gmail.com>
2024-03-15 22:08:58 +0100

    Add tests for deployment reconcile

--- configmap_test.go
+++ configmap_test.go
@@ -18,68 +18,72 @@ const (
 
 var _ = Describe("ConfigMap", func() {
 	AfterEach(func() {
-		err := k8sClient.DeleteAllOf(context.Background(), &corev1.ConfigMap{}, client.InNamespace("default"))
+		err := k8sClient.DeleteAllOf(context.Background(), &corev1.ConfigMap{}, client.InNamespace(namespace))
 		Expect(err).ToNot(HaveOccurred())
 	})
 
-	It("creates a new ConfigMap", func() {
-		configMap := &corev1.ConfigMap{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      configMapName,
-				Namespace: namespace,
-			},
-		}
+	Context("when owner is valid", func() {
+		It("creates a new ConfigMap", func() {
+			configMap := &corev1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      configMapName,
+					Namespace: namespace,
+				},
+			}
 
-		result, err := utils.ReconcileConfigMap(context.Background(), configMap, owner, k8sClient, testEnv.Scheme)
-		Expect(err).ToNot(HaveOccurred())
-		Expect(result).To(BeNil())
+			result, err := utils.ReconcileConfigMap(context.Background(), configMap, owner, k8sClient, testEnv.Scheme)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(result).To(BeNil())
+		})
+
+		It("updates an outdated ConfigMap", func() {
+			configMap := &corev1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      configMapName,
+					Namespace: namespace,
+				},
+				Data: map[string]string{
+					"sample": "data",
+				},
+			}
+
+			result, err := utils.ReconcileConfigMap(context.Background(), configMap, owner, k8sClient, testEnv.Scheme)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(result).To(BeNil())
+
+			updatedConfigMap := &corev1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      configMapName,
+					Namespace: namespace,
+				},
+				Data: map[string]string{
+					"sample": "newData",
+				},
+			}
+
+			result, err = utils.ReconcileConfigMap(context.Background(), updatedConfigMap, owner, k8sClient, testEnv.Scheme)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(result).To(BeNil())
+		})
 	})
 
-	It("updates an outdated ConfigMap", func() {
-		configMap := &corev1.ConfigMap{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      configMapName,
+	Context("when owner is invalid", func() {
+		It("failes to create a new ConfigMap", func() {
+			configMap := &corev1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      configMapName,
+					Namespace: namespace,
+				},
+			}
+
+			invalidOwner := &metav1.ObjectMeta{
+				Name:      "invalidOwner",
 				Namespace: namespace,
-			},
-			Data: map[string]string{
-				"sample": "data",
-			},
-		}
+			}
 
-		result, err := utils.ReconcileConfigMap(context.Background(), configMap, owner, k8sClient, testEnv.Scheme)
-		Expect(err).ToNot(HaveOccurred())
-		Expect(result).To(BeNil())
-
-		updatedConfigMap := &corev1.ConfigMap{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      configMapName,
-				Namespace: namespace,
-			},
-			Data: map[string]string{
-				"sample": "newData",
-			},
-		}
-
-		result, err = utils.ReconcileConfigMap(context.Background(), updatedConfigMap, owner, k8sClient, testEnv.Scheme)
-		Expect(err).ToNot(HaveOccurred())
-		Expect(result).To(BeNil())
-	})
-
-	It("failes to create a new ConfigMap with invalid owner", func() {
-		configMap := &corev1.ConfigMap{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      configMapName,
-				Namespace: namespace,
-			},
-		}
-
-		invalidOwner := &metav1.ObjectMeta{
-			Name:      "invalidOwner",
-			Namespace: namespace,
-		}
-
-		result, err := utils.ReconcileConfigMap(context.Background(), configMap, invalidOwner, k8sClient, testEnv.Scheme)
-		Expect(err).To(HaveOccurred())
-		Expect(result).To(BeNil())
+			result, err := utils.ReconcileConfigMap(context.Background(), configMap, invalidOwner, k8sClient, testEnv.Scheme)
+			Expect(err).To(HaveOccurred())
+			Expect(result).To(BeNil())
+		})
 	})
 })
